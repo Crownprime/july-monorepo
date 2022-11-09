ba32bcec259afed3d43891ccb59caa0b152394cf
jiangxujin <jiangxujin@bytedance.com>
2022-11-09 14:49:39 +0800

    fix: build failed

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', '@july_cm'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@next/next/recommended',
+    '@july_cm'
+  ],
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': 0
   }
