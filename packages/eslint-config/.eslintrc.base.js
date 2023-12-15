7b285136d1282bcb31f007325e9daaad67bf2f79
jiangxujin <jiangxujin@bytedance.com>
2023-12-15 17:59:14 +0800

    feat: publish test

--- packages/eslint-config/.eslintrc.base.js
+++ packages/eslint-config/.eslintrc.base.js
@@ -12,7 +12,7 @@ module.exports = {
       presets: ['@babel/preset-env', '@babel/preset-react'],
     },
   },
-  ignorePatterns: ['**/node_modules', '**/dist', '**/!(package).json', 'vercel.json'],
+  ignorePatterns: ['**/node_modules', '**/dist', '!**/**.json', '!package.json'],
   rules: {
     'prettier/prettier': [
       'warn',
---
+++ packages/rc-hooks/CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log - @july_cm/rc-hooks
+
+This log was last generated on Fri, 15 Dec 2023 08:19:55 GMT and should not be manually modified.
+
+## 0.0.2
+Fri, 15 Dec 2023 08:19:55 GMT
+
+### Patches
+
+- test publish
+
